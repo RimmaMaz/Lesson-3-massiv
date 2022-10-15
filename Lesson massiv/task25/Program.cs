ade843faaf59ee98353ead5242278421dee75818
RIMMA <mazeevar2912@gmail.com>
2022-10-15 19:47:36 +0500

    124586

---
+++ Lesson massiv/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+int Getarray(int A, int B)
+{
+    int result = 1;
+    for (int i=1; i<=B; i++)
+    {
+        result=result*A;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите первое число ...");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число ...");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write(Getarray(a, b));
+
