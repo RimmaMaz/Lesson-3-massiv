a5cfd90ec2e146b991a6c2c8ae377a4b901273f0
RIMMA <mazeevar2912@gmail.com>
2022-10-15 20:52:04 +0500

    task27

---
+++ Lesson massiv/task27/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int Sum(int a)
+{
+    int result = 0;
+
+    while (a > 0)
+    {
+        result = result + (a % 10);
+        a = a / 10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число ...");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {A} равна {Sum(A)}");
+
+
+
+
