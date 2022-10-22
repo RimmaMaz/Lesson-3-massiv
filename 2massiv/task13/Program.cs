26fa1848ed7035e1b346b071ecc6f4164d5e0b18
RIMMA <mazeevar2912@gmail.com>
2022-10-22 21:25:02 +0500

    task 13

---
+++ 2massiv/task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int a1 = a/10;
+int num = a;
+
+if (a1>10 || a1<-10)
+{
+    while (a>999 || a<-999)
+    {
+        a = a/10;
+    }
+    if (a<0)
+    {
+        a=-a;
+        Console.WriteLine("Третья цbфра трехзначного числа {num} -> {a%10} ");
+    }
+    else
+    {
+        Console.WriteLine("Третья цbфра трехзначного числа {num} -> {a%10} ");
+    }
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
